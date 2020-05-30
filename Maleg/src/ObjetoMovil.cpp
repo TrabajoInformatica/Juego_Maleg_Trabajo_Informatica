b7f9cdd90a7f7b3a89a8ecfb3537cb179b3d3929
tiranin <adriansanchezatienza@gmail.com>
2020-05-30 22:05:43 +0200

    Sobrecarga OPER. fixed

--- Maleg/src/ObjetoMovil.cpp
+++ Maleg/src/ObjetoMovil.cpp
@@ -2,28 +2,21 @@
 
 void ObjetoMovil::Mueve(float t)
 {
-	posicion.x = posicion.x + velocidad.x * t + 0.5f * aceleracion.x * t * t;
-	posicion.y = posicion.y + velocidad.y * t + 0.5f * aceleracion.y * t * t;
-	velocidad.x = velocidad.x + aceleracion.x * t;
-	velocidad.y = velocidad.y + aceleracion.y * t; 
-
-	// JAJAJAJAJ luis chavalito
-// sdflkhasfñasdkf jklshdds 
-	// el bromas
-	// jaja xd
-	/*posicion = posicion + velocidad * t + aceleracion * (0.5f * t * t);
-	velocidad = velocidad + aceleracion * t;*/
-
+	posicion = posicion + velocidad * t + aceleracion * (0.5f * t * t);
+	velocidad = velocidad + aceleracion * t;
 }
+
 void  ObjetoMovil::SetAce(float ax,float ay) {
 	aceleracion.x = ax;
 	aceleracion.y = ay;
 }
+
 void ObjetoMovil::SetPos(float x, float y)
 {
 	posicion.x = x;
 	posicion.y = y;
 }
+
 void ObjetoMovil::SetVel(float vx, float vy)
 {
 	velocidad.x = vx;
--- Maleg/src/Vector2D.cpp
+++ Maleg/src/Vector2D.cpp
@@ -28,14 +28,14 @@ Vector2D Vector2D::unitario() {
 	return retorno;
 }
 
-Vector2D Vector2D::operator - (Vector2D& v) {
+Vector2D Vector2D::operator - (const Vector2D& v) {
 	Vector2D res;
 	res.x = x - v.x;
 	res.y = y - v.y;
 	return res;
 }
 
-Vector2D Vector2D::operator + (Vector2D& v) {
+Vector2D Vector2D::operator + (const Vector2D& v) {
 	Vector2D res;
 	res.x = x + v.x;
 	res.y = y + v.y;
