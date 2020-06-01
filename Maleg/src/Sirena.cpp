a85aee1f51393c93a849325bb98d01c50ed6171e
tiranin <adriansanchezatienza@gmail.com>
2020-06-01 23:43:55 +0200

    correr con escudo

--- Maleg/src/Sirena.cpp
+++ Maleg/src/Sirena.cpp
@@ -9,7 +9,7 @@ Sirena::Sirena() {
 	rojo = 0;
 	azul = 100;
 	altura = 1.0f;
-	salto_max = 8.0f;
+	salto_max = 15.0f;
 	salto_min = 3.0f;
 	velocidad.y = 2.0;
 }
@@ -30,14 +30,14 @@ void Sirena::Salto() {
 	if (posicion.y < salto_min) {
 
 		posicion.y = salto_min;
-		velocidad.y = 2.0f;
+		velocidad.y = 10.0f;
 		aceleracion.y = 0.5f;
 	}
 
 	else if (posicion.y > salto_max) {
 
 		posicion.y = salto_max;
-		velocidad.y = -2.0f;
+		velocidad.y = -10.0f;
 		aceleracion.y = 0.5f;
 	}
 
